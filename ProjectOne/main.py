12514988790f72a85d8c81991f8aa2afcf4f5809
Andrew Welch <abw28@zips.uakron.edu>
2019-02-18 13:49:30 -0500

    Adding main.py

---
+++ ProjectOne/main.py
@@ -0,0 +1,5 @@
+def displayMenu():
+    print("Select an available option:")
+    print("1. Learn a decision tree from training data.")
+    print("2. Save the tree learned in menu item 1.")
+    print("")
