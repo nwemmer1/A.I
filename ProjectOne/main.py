b65b9b573c9103e188e2d4096fd18bdc919525ed
Andrew Welch <abw28@zips.uakron.edu>
2019-02-18 13:59:32 -0500

    Created menu function

--- ProjectOne/main.py
+++ ProjectOne/main.py
@@ -2,4 +2,10 @@ def displayMenu():
     print("Select an available option:")
     print("1. Learn a decision tree from training data.")
     print("2. Save the tree learned in menu item 1.")
-    print("")
+    print("3. Applying the decision tree to new cases.")
+    print("4. Load a tree model saved previously and apply the model to new cases interactively as in menu item 3")
+    print("5. Quit")
+
+
+displayMenu()
+# def collectAndRedirect():
